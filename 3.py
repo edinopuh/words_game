8f4e06973ba8737c0f70d05d8a8dfbee2e6a8358
Margaret <margaret.baskakova@mail.ru>
2026-06-17 23:17:45 +0300

    files

--- 3.py
+++ 3.py
@@ -20,7 +20,7 @@ with open('cities.txt', mode='r', encoding='utf-8') as f:
 
 @app.route('/cities', methods=['GET', 'POST'])
 def cities_game():
-    # Инициализируем сессию для игры в города, если её ещё нет
+    # Инициализация сессионных данных при первом заходе
     if 'cities_message_list' not in session:
         session['cities_message_list'] = []
     if 'last_word' not in session:
@@ -36,7 +36,7 @@ def cities_game():
         last_word = session['last_word']
 
         if last_word:
-            if last_word[-1] == 'ь' or last_word[-1] == 'ы':
+            if last_word[-1] in ['ь', 'ы']:
                 letter0 = last_word[-2]
             else:
                 letter0 = last_word[-1]
@@ -47,7 +47,7 @@ def cities_game():
             session['cities_message_list'].append(
                 (f'Твой город должен начинаться не на букву "{word[0]}", а на букву "{letter0}".', 1))
         else:
-            if word[-1] == 'ь' or word[-1] == 'ы':
+            if word[-1] in ['ь', 'ы']:
                 letter = word[-2]
             else:
                 letter = word[-1]
@@ -59,9 +59,9 @@ def cities_game():
             else:
                 new_word = random.choice(words_to_choose)
                 session['cities_message_list'].append((new_word, 1))
-                session['last_word'] = new_word
-        form.answer.data = ''
+                session['last_word'] = new_word  # Сохраняем в сессию
 
+        form.answer.data = ''
     return render_template('chat.html',
                            message_list=session['cities_message_list'],
                            form=form,
@@ -74,23 +74,25 @@ end_game_message = ['покупаю', 'беру', 'покупаю слона', '
 
 @app.route('/elephant', methods=['GET', 'POST'])
 def elephant():
-    # Инициализируем сессию для игры «Купи слона», если её ещё нет
+    # Инициализация сессионных данных
     if 'elephant_message_list' not in session:
         session['elephant_message_list'] = [('Привет! Купи слона.', 1)]
+    if 'last_elephant_message' not in session:
+        session['last_elephant_message'] = ''
     if 'elephant_is_finished' not in session:
         session['elephant_is_finished'] = False
 
     form = AnswerForm()
     if form.validate_on_submit():
-        last_elephant_message = form.answer.data
-        session['elephant_message_list'].append((last_elephant_message, 0))
-        if last_elephant_message.lower() in end_game_message:
+        session['last_elephant_message'] = form.answer.data
+        session['elephant_message_list'].append((session['last_elephant_message'], 0))
+        if session['last_elephant_message'].lower() in end_game_message:
             session['elephant_message_list'].append(('Молодец! 🐘', 1))
             session['elephant_is_finished'] = True
         else:
-            session['elephant_message_list'].append((f'Все говорят "{last_elephant_message}", а ты купи слона!', 1))
+            session['elephant_message_list'].append(
+                (f'Все говорят "{session["last_elephant_message"]}", а ты купи слона!', 1))
         form.answer.data = ''
-
     return render_template('chat.html',
                            message_list=session['elephant_message_list'],
                            form=form,
